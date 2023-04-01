d91f4d4ea394ba409500fc14bad43820512618de
Angel De La Cruz <angelfranciscoprivado@gmail.com>
2023-03-31 21:30:11 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <p className='read-the-docs'>Hello Vite is working</p>
+    </div>
+  );
+}
+
+export default App;
